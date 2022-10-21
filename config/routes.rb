ccdb89495f197f8950f68e42c0b2797c58ce26b2
reikopi <rk02.stmi.1018@gmail.com>
2022-10-21 14:28:50 +0000

    [Add]gohan_dosuru_memberコントローラーの追加、フッターロゴの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  # get 'homes/top'
   get 'groups/new'
   get 'groups/show'
   get 'groups/edit'
-  get 'new/show'
-  get 'new/edit'
-  devise_for :members
+  resources :members, only: [:show]
+  get 'members/success' => 'members#success'
+
+  devise_for :members,skip: [:passwords], controller: {
+    registrations: "registrations",
+    sessions: 'sessions'
+  }
+
+  root to: 'homes#top'
+
+  # resources :members, only: [:show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
